929ca945348a54123fabd28f899e4c6c7d3c7165
631994449 <45552074+631994449@users.noreply.github.com>
2020-05-19 14:47:17 +0800

    First Commit

---
+++ src/main/java/cn/edu/hziee/peisp/PeispApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hziee.peisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeispApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeispApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/hziee/peisp/PeispApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hziee.peisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeispApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
