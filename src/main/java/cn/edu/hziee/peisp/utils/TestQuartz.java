abf076a1da578d0462e18451e44dadbae8b96827
631994449 <45552074+631994449@users.noreply.github.com>
2020-08-18 16:51:44 +0800

    0818

--- src/main/java/cn/edu/hziee/peisp/utils/TestQuartz.java
+++ src/main/java/cn/edu/hziee/peisp/utils/TestQuartz.java
@@ -6,9 +6,6 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 
 public class TestQuartz extends QuartzJobBean{
 
-    private static final String MACHINE_GOTO_LEFT = "a";
-
-    private static final String MACHINE_GOTO_RIGHT = "b";
     /**
      * 执行定时任务
      * @param jobExecutionContext
@@ -18,15 +15,14 @@ public class TestQuartz extends QuartzJobBean{
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         //发送向右移动信号
-        ServerManager.broadCast(MACHINE_GOTO_LEFT);
+
         //设置延时
         try {
-            Thread.sleep(5000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         //发送向左移动信号
-        ServerManager.broadCast(MACHINE_GOTO_RIGHT);
 
     }
 }
